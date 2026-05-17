2fa4de65ac8fc8769249d9f0c6d8d666fd612066
cwyii <160295905+cwyii@users.noreply.github.com>
2026-05-17 18:11:53 -0500

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,23 +1,66 @@
-        function audioPlayer(){
-            var currentSong = 0;
-            $("#audioPlayer")[0].src = $("#playlist li a")[0];
-            $("#audioPlayer")[0].play();
-            $("#playlist li a").click(function(e){
-               e.preventDefault(); 
-               $("#audioPlayer")[0].src = this;
-               $("#audioPlayer")[0].play();
-               $("#playlist li").removeClass("current-song");
-                currentSong = $(this).parent().index();
-                $(this).parent().addClass("current-song");
+function audioPlayer() {
+    var currentSong = 0;
+    var audio = $("#audioPlayer")[0];
+    var playlist = $("#playlist li a");
+
+    function updateMediaSession() {
+        if ("mediaSession" in navigator) {
+            var songTitle = $(playlist[currentSong]).text();
+
+            navigator.mediaSession.metadata = new MediaMetadata({
+                title: songTitle,
+                artist: "Your Artist Name"
             });
-            
-            $("#audioPlayer")[0].addEventListener("ended", function(){
-               currentSong++;
-                if(currentSong == $("#playlist li a").length)
-                    currentSong = 0;
-                $("#playlist li").removeClass("current-song");
-                $("#playlist li:eq("+currentSong+")").addClass("current-song");
-                $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
-                $("#audioPlayer")[0].play();
+
+            navigator.mediaSession.setActionHandler("nexttrack", function () {
+                playSong(currentSong + 1);
             });
+
+            navigator.mediaSession.setActionHandler("previoustrack", function () {
+                playSong(currentSong - 1);
+            });
+
+            navigator.mediaSession.setActionHandler("play", function () {
+                audio.play();
+            });
+
+            navigator.mediaSession.setActionHandler("pause", function () {
+                audio.pause();
+            });
+
+            navigator.mediaSession.setActionHandler("seekbackward", null);
+            navigator.mediaSession.setActionHandler("seekforward", null);
         }
+    }
+
+    function playSong(index) {
+        if (index >= playlist.length) {
+            index = 0;
+        }
+
+        if (index < 0) {
+            index = playlist.length - 1;
+        }
+
+        currentSong = index;
+
+        $("#playlist li").removeClass("current-song");
+        $("#playlist li:eq(" + currentSong + ")").addClass("current-song");
+
+        audio.src = playlist[currentSong].href;
+        audio.play();
+
+        updateMediaSession();
+    }
+
+    playSong(currentSong);
+
+    $("#playlist li a").click(function (e) {
+        e.preventDefault();
+        playSong($(this).parent().index());
+    });
+
+    audio.addEventListener("ended", function () {
+        playSong(currentSong + 1);
+    });
+}
